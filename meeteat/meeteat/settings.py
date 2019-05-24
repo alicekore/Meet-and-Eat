c08b84d8d3fc325197f89bf0370fb2411201c93c
hobergma <claasiew159>
2019-05-24 20:19:56 +0200

    -added PostgreSQL and quick guide

--- meeteat/meeteat/settings.py
+++ meeteat/meeteat/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'meeteat.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'meet_and_eat',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
