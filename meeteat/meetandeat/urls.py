d3248931b5bbb2d0bb88aeefb5d97052f76501ee
hobergma <claasiew159>
2019-05-26 00:11:27 +0200

    -added login template + function -added logout template + function

--- meeteat/meetandeat/urls.py
+++ meeteat/meetandeat/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -6,4 +7,7 @@ app_name = 'meetandeat'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+
 ]
