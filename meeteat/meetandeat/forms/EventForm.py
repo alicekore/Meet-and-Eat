5342ab479431f44554f9584106fcc68b09bf6afe
loposkin <loposkin@gmail.com>
2019-05-27 18:08:09 +0200

    fixed event form

--- meeteat/meetandeat/forms/EventForm.py
+++ meeteat/meetandeat/forms/EventForm.py
@@ -4,10 +4,9 @@ from django.core.exceptions import ValidationError
 from django.forms.widgets import DateTimeInput
 
 
-
 class EventForm(forms.Form):
-    event_name = forms.CharField(label='eventName', max_length=50)
-    description = forms.CharField(label='eventDescription', max_length=50, required=False)
-    place = forms.CharField(label='eventPlace', max_length=50, required=False)
-    person_number = forms.IntegerField(label="personNumber", min_value=2)
-    datetime = forms.DateTimeField(label="datetime")
+    event_name = forms.CharField(label='eventName', max_length=40)
+    description = forms.CharField(label='eventDescription', max_length=160, required=False)
+    place = forms.CharField(label='eventPlace', max_length=30, required=False)
+    person_number = forms.IntegerField(label="personNumber", min_value=2, max_value=16)
+    datetime = forms.DateTimeField(label="datetime", input_formats=['%Y-%m-%dT%H:%M'])
