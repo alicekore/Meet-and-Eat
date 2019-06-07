b7ebe5393e94cd53823dc38bebfddd94b97f4afb
Deniz <deniz.scholz@t-online.de>
2019-06-07 22:08:02 +0200

    Added Select2 Widget for Tags
    Use pip install django_select2

--- meeteat/meetandeat/forms/EventForm.py
+++ meeteat/meetandeat/forms/EventForm.py
@@ -1,12 +1,12 @@
 from django import forms
 from meetandeat.models import Event, Tag
+from django_select2.forms import Select2MultipleWidget
 
 
 class EventForm(forms.ModelForm):
     datetime = forms.DateTimeField(label="datetime", input_formats=['%Y-%m-%dT%H:%M'])
-
     tags_queryset = Tag.objects.filter(approved=True)
-    tags = forms.ModelMultipleChoiceField(queryset=tags_queryset, widget=forms.CheckboxSelectMultiple)
+    tags = forms.ModelMultipleChoiceField(queryset=tags_queryset, widget=Select2MultipleWidget())
 
     class Meta:
         model = Event
