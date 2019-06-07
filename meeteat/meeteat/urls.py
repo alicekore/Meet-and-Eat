08381fa270435deb6784dda83211aaa796d3dfeb
Deniz <deniz.scholz@t-online.de>
2019-06-07 22:08:02 +0200

    Added Select2 Widget for Tags
    Use pip install django_select2

--- meeteat/meeteat/urls.py
+++ meeteat/meeteat/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('meetandeat.urls')),
     path('admin/', admin.site.urls),
+    url(r'^select2/', include('django_select2.urls')),
 ]
