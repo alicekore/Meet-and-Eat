35d31a766ed61f9d5b104bccdab3ca851b478d37
niwie <n.wiedersich@campus.tu-berlin.de>
2019-06-09 21:22:56 +0200

    added join, leave button, db func, views accordingly and mixings for permission handling

--- meeteat/meetandeat/admin.py
+++ meeteat/meetandeat/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Event,User,AbstractUser,Group
+from .models import Event,User,AbstractUser
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(User)
-admin.site.register(Group)
