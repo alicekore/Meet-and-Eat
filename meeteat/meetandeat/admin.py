1238850c42c6a9455176471162cb7cae1a62a283
Emina Hrustic <amira.hrustic@gmx.de>
2019-06-11 09:41:40 +0200

    Merge branch 'registerUserPage' of https://github.com/alicekore/Meet-and-Eat into registerUserPage

--- meeteat/meetandeat/admin.py
+++ meeteat/meetandeat/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event,User,AbstractUser
 
 # Register your models here.
 admin.site.register(Event)
+admin.site.register(User)
+
